df1259a0c8cf9ac01fda1e88fd1eecdafc442898
enginehenryed <enginehenryed@gmail.com>
2016-10-30 17:00:44 +0900

    Remove 'devtool' key which appears twice

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ var BUILD_DIR = path.resolve(__dirname, 'build');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  devtool: "source-map",
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
