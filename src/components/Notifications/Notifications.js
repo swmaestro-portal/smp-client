76744ac288aa7d10d3ba82031bb02c3a13963d35
enginehenryed <enginehenryed@gmail.com>
2016-11-12 02:30:15 +0900

    Add Notifications

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -2,38 +2,54 @@
 import React from 'react'
 import classNames from 'classnames'
 import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
 
 /* Internal Dependencies */
-import styles from './LeftBar.scss'
-import MainMenu from '../MainMenu'
-import UserInfo from '../UserInfo'
-import Notifications from '../Notifications'
+import styles from './Notifications.scss'
 
-class LeftBar extends React.Component {
+
+class Notifications extends React.Component {
 
   constructor() {
     super()
-    this.handleClick = this.handleClick.bind(this)
   }
-
-  handleClick(event) {
-    this.props.router.push('/signup')
+  
+  componentWillMount() {
+    // this.props.dispatch()
   }
 
   render() {
+    // console.log(1, this.props.reduxState, notificationsApi)
     return (
-      <div
-        className={classNames(styles.wrapper, this.props.className)}>
-        <div className={styles.content}>
-          <UserInfo/>
-          <MainMenu/>
-          <Notifications/>
-        </div>
+      <div className={styles.wrapper}>
+        <ul>
+          <li>
+            <i className="fa fa-bell" aria-hidden="true"/>
+            <span>foo로 부터 댓글이 달렸습니다</span>
+          </li>
+          <li>
+            <i className="fa fa-bell" aria-hidden="true"/>
+            <span>foo로 부터 댓글이 달렸습니다</span>
+          </li>
+          <li>
+            <i className="fa fa-bell" aria-hidden="true"/>
+            <span>foo로 부터 댓글이 달렸습니다</span>
+          </li>
+        </ul>
       </div>
     )
   }
 }
 
-const routedLeftbar = withRouter(LeftBar)
 
-export default routedLeftbar
+const mapStateToProps = (state/*, props*/) => {
+  return {
+    reduxState: state
+  }
+}
+
+const routedNotifications = withRouter(Notifications)
+const connectedNotifications = connect(mapStateToProps)(routedNotifications)
+
+// export default routedNotifications
+export default connectedNotifications
