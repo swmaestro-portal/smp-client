a4cd9ac32a95c7f434f8c56ef3de1a2b444657db
jangjunha <jangjunha113@gmail.com>
2016-11-21 01:06:33 +0900

    Implement UserInfo container

--- src/containers/UserInfoContainer/UserInfoContainer.js
+++ src/containers/UserInfoContainer/UserInfoContainer.js
@@ -1,10 +1,12 @@
 /* External Dependencies */
 import React from 'react'
 import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
 
 /* Internal Dependencies */
 import styles from './UserInfoContainer.scss'
 import Avatar from '../../components/Avatar'
+import { formatGenerationId, formatGroupCode } from '../../utils/FormatUtils'
 
 class UserInfoContainer extends React.Component {
 
@@ -19,16 +21,26 @@ class UserInfoContainer extends React.Component {
   }
 
   render() {
+    const lastGroup = this.props.me.get('lastGroup')
+
+    const groupInfo = formatGroupCode(lastGroup.get('groupCode'))
+    let generationInfo = formatGenerationId(lastGroup.get('generationId')) + ' '
+
+    console.log(9000000, lastGroup)
+    if (lastGroup.get('groupCode') == 2) {
+      generationInfo = ''
+    }
+
     return (
       <div className={styles.wrapper}>
         <Avatar/>
         <div className={styles.info}>
           <p>
-            <span>7기</span>
-            <span>장준하</span>
+            <span>{generationInfo}</span>
+            <span>{this.props.me.get('userName')}</span>
           </p>
           <p>
-            <span>멘티</span>
+            <span>{groupInfo}</span>
           </p>
         </div>
         <button onClick={this.handleClickSignout}>
@@ -40,6 +52,13 @@ class UserInfoContainer extends React.Component {
 
 }
 
+const mapStateToProps = (state/*, props*/) => {
+  return {
+    me: state.meReducer.me
+  }
+}
+
 UserInfoContainer = withRouter(UserInfoContainer)
+UserInfoContainer = connect(mapStateToProps)(UserInfoContainer)
 
 export default UserInfoContainer
