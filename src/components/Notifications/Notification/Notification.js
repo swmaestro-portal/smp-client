b2c734b7730a104e70a9e62347ca817e14b04c91
jangjunha <jangjunha113@gmail.com>
2016-11-20 18:05:30 +0900

    Use moment insted of react-timeago

--- src/components/Notifications/Notification/Notification.js
+++ src/components/Notifications/Notification/Notification.js
@@ -2,24 +2,21 @@
 import React from 'react'
 import { withRouter } from 'react-router'
 import classNames from 'classnames'
-import TimeAgo from 'react-timeago'
-import koreanStrings from 'react-timeago/lib/language-strings/ko'
-import buildFormatter from 'react-timeago/lib/formatters/buildFormatter'
+import moment from 'moment'
 
 /* Internal Dependencies */
 import styles from './Notification.scss'
 import Link from '../../Link'
 
-const formatter = buildFormatter(koreanStrings)
+moment.locale('ko')
 
 const Notification = (props) => {
-
   return (
     <li className={classNames(styles.wrapper, props.notification.get('notificationIsRead') ? styles.read : styles.unread )}>
       <div onClick={() => props.handleClickNotification(props.notification.get('notificationId'))}>
         <Link to={props.notification.get('notificationUrl')} className={styles.link}>
           <p className={styles.message}>{props.notification.get('notificationMessage')}</p>
-          <TimeAgo date={props.notification.get('notificationCreatedAt')} formatter={formatter} className={styles.timeago} />
+          <p className={styles.timeago}>{moment(props.notification.get('notificationCreatedAt')).fromNow()}</p>
         </Link>
       </div>
     </li>
