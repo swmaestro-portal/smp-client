193fce8a7b2176ebb024536b0d3fab91379d3772
dbgsprw <dbgsprw@gmail.com>
2016-11-21 17:53:28 +0900

    add search apis

--- src/apis/SearchApi.js
+++ src/apis/SearchApi.js
@@ -1,3 +1,7 @@
-/**
- * Created by Yoo on 2016-11-21.
- */
+import * as API from './lib.js'
+
+export default {
+  requestGetAll: function(query) {
+    return API.get(`search/all?q=` + query)
+  }
+}
